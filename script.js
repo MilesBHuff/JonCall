65b8462bb695690768b324f6e241fb83ceafe1d7
Miles B Huff <MilesBHuff@Users.NoReply.Github.com>
2017-10-13 03:08:48 -0400

    The buttons to show/hide the view/edit screens work now.

--- script.js
+++ script.js
@@ -3,18 +3,37 @@
 
 // GUI functions
 // ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+
+// btnDownload
+// =============================================================================
+/** Sends the latest .csvs to the user. **/
 function btnDownload() {
+	//TODO
+} //function
 
-}
+// btnEdit
+// =============================================================================
+/** Hides the tables and shows the edit forms. **/
 function btnEdit() {
+	document.getElementById("divNotCalled").style.display = "none";
+	document.getElementById("divYesCalled").style.display = "none";
+	document.getElementById("frmEntry"    ).style.display = "inline-grid";
+	document.getElementById("frmUpload"   ).style.display = "inline-grid";
+} //function
 
-}
+// btnView
+// =============================================================================
+/** Hides the edit forms and shows the tables. **/
 function btnView() {
-
-}
+	document.getElementById("frmEntry"    ).style.display = "none";
+	document.getElementById("frmUpload"   ).style.display = "none";
+	document.getElementById("divNotCalled").style.display = "inline-grid";
+	document.getElementById("divYesCalled").style.display = "inline-grid";
+} //function
 
 // main()
 // ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+/** The main function. **/
 function main() {
-
+	//TODO
 } //function
